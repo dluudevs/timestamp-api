0fe6bed084e15bcb81502df7a2e76736b1a79958
Derek <derekluu90@gmail.com>
2020-07-30 23:03:11 -0400

    refactor route to handle empty parameter\

--- src/app.js
+++ src/app.js
@@ -3,20 +3,21 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-// How would this look like if moment.js is used?
 // Add a basic web page with input field and call API
 // Use hbs and create basic Footer / Header template 
 // Create about view to describe the app
 
-app.get('/api/timestamp/:date_string', (req, res) => {
+// question mark after param means it's optional. this route accepts an empty date_string param
+app.get('/api/timestamp/:date_string?', (req, res) => {
   const dateParam = req.params.date_string
-  const dateParse = dateParam.match(/\d{4}-\d{2}-\d{2}/gm) ? Date.parse(dateParam) : Number(dateParam)
-  const date = new Date(dateParse)
-
+  const dateParse = () => dateParam.match(/\d{4}-\d{2}-\d{2}/gm) ? Date.parse(dateParam) : Number(dateParam)
+  
+  const date = !dateParam ? new Date() : new Date(dateParse())
   const dateUnix = date.getTime()
   const dateUTC = date.toUTCString()
-
-  if(isNaN(dateParse) || !dateUnix || dateUTC === "Invalid Date"){
+  
+  // this will also handle invalid date_string formats
+  if(!dateUnix || dateUTC === "Invalid Date"){
     return res.send({ error: 'Invalid Date'})
   }
 
